a89fda11a2d41eeaacf5c32fbb6ee8c910a3d628
cyisfor <nobody>
2016-02-16 22:53:46 +0000

    auto (/extra/user/code/regularly/calendar.c) 65 1199

--- calendar.c
+++ calendar.c
@@ -2,13 +2,6 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
-/*  http://www.rosettacode.org/wiki/Leap_year#C */
-bool is_leap_year(int year)
-{
-  year += 1900;
-  return (!(year % 4) && year % 100 || !(year % 400)) ? 1 : 0;
-}
-
 uint8_t mdays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 void clock_localtime_r(struct timespec* time, struct tm* dest) {
@@ -17,22 +10,7 @@ void clock_localtime_r(struct timespec* time, struct tm* dest) {
 struct tm* clock_localtime(struct timespec* base) {
   static tm mytm; // so we can pass it to localtime_r elsewhere
   clock_localtime_r(base,&mytm);
-  return &mytm;  
-}
-
-static void incmonth(struct tm* base) {
-	if(base->tm_mon == 11) {
-	  base->tm_year += 1;
-	  base->tm_mon = 0;
-	} else {
-	  ++base->tm_mon;
-	}
-}
-void advance_months(struct tm* base, uint32_t months) {
-  uint32_t i;
-  for(i=0;i<months;++i) {
-	incmonth(base);
-  }
+  return &mytm;
 }
 
 static void inc_year(struct tm* base) {
@@ -54,22 +32,30 @@ INCTHING(hour,mday,0,23);
 INCTHING(min,hour,0,59);
 INCTHING(sec,min,0,59);
 
-static 
-void advance_days(struct tm* base, uint32_t days) {
+static void inc_week(struct tm* base) {
+  uint8_t i;
+  for(i=0;i<7;++i)
+	inc_day(base);
+}
+
+void advance_time(struct timespec* dest, enum time_unit unit, uint32_t increment) {
+  struct tm* base = clock_localtime(dest);
   uint32_t i;
-  for(i=0;i<days;++i) {
-	if(base->tm_mday == mdays[base.tm_mon]) {
-	  if(base->tm_mon==11) {
-		base->tm_year += 1;
-		base->tm_mon = 0;
-	  } else {
-		++base->tm_mon;
-	  }
-	}
-	  base->tm_mon = 0;
-	} else {
-	  ++base->tm_mon;
-	}
-	// this adjusts for leap years, leap seconds, etc
-	localtime_r(mktime(base),base);
-  }
+  for(i=0;i<increment;++i) {
+	switch(unit) {
+#define ONE(what,how)							\
+	  case what:								\
+		inc_ ## how(base);						\
+		continue
+	  ONE(SECONDS,sec);
+	  ONE(MINUTES,min);
+	  ONE(HOURS,hour);
+	  ONE(DAYS,mday);
+	  ONE(WEEKS,week);
+	  ONE(MONTHS,month);
+	  ONE(YEARS,year);
+	  default:
+		error("argh!");
+	};
+  };
+}
--- calendar.h
+++ calendar.h
@@ -1,3 +1,3 @@
-enum time_unit { SECONDS, HOURS, MINUTES, DAYS, MONTHS, YEARS };
+enum time_unit { SECONDS, HOURS, MINUTES, DAYS, WEEKS, MONTHS, YEARS };
 
 advance_time(struct timespec* base, time_unit unit, uint32_t increment);
