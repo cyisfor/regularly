c0d91a65cb11df2167cc744e7ffb14b5a32fe87e
cyisfor <autocommit>
2017-08-01 21:04:15 +0000

    auto 0 21 368

--- main.c
+++ main.c
@@ -67,15 +67,12 @@ void parse_interval(struct tm* dest,
 		.s = s,
 		.len = len,
   };
-	memset(dest,0,sizeof(*dest));
-	info("before: %s",ctime_interval(dest));
   memset(dest,0,sizeof(struct tm));
   while(next_token(&ctx)) {
 		if(ctx.state == SEEKNUM) {
 			advance_interval(dest,&ctx.interval);
 		}
   }
-	info("after: %s",ctime_interval(dest));
 }
 
 struct rule {
