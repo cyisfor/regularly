f543712a5294aae1094143e03d764edada59ade8
cyisfor <autocommit>
2017-08-01 19:33:26 +0000

    auto 0 60 611

--- calendar.c
+++ calendar.c
@@ -11,8 +11,7 @@
 	ONE(mon,"month");															\
 	ONE(year,"year");
 
-const char* ctime_interval(struct tm* interval) {
-	static char buf[0x100];
+bool ctime_interval_r(struct tm* interval, char* buf, size_t len) {
 	ssize_t offset = 0;
 	bool first = true;
 #define ONE(what,name)													\
@@ -20,26 +19,41 @@ const char* ctime_interval(struct tm* interval) {
 		if(first) {																	\
 			first = false;														\
 		} else {																		\
+			if(offset+2>=len) return false;						\
 			buf[offset] = ',';												\
 			buf[offset+1] = ' ';											\
 			offset += 2;															\
 		}																						\
 		offset+=snprintf(buf+offset,								\
-										 0x100-offset,							\
+										 len-offset,								\
 										 "%d " name,								\
 										 interval->tm_ ## what);		\
+		if(offset >= len) return false;							\
 		if(interval->tm_ ## what > 1) {							\
+			if(offset == len) return false;						\
 			buf[offset] = 's';												\
 			++offset;																	\
 		}																						\
 	}
 	FOR_TM;
 #undef ONE
+	if(offset != len) 
+		buf[offset] = '\0';
+	return true;
+}
+
+const char* ctime_interval(struct tm* interval) {
+	static char* buf = malloc(0x100);
+	static size_t len = 0x100;
+
+	while(false == ctime_interval_r(interval, buf, len)) {
+		len += 0x100;
+		buf = realloc(buf,len);
+	}
 	return buf;
 }
 
-
-	void advance_interval(struct tm* dest, struct tm* interval) {
+void advance_interval(struct tm* dest, struct tm* interval) {
 #define ONE(what,name)													\
 	dest->tm_ ## what += interval->tm_ ## what;
 	FOR_TM;
