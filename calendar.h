ecd592e05a35c151e8a0a2c22a908db37be1f94e
cyisfor <autocommit>
2017-08-01 19:34:20 +0000

    auto 0 13 118

--- calendar.h
+++ calendar.h
@@ -1,5 +1,8 @@
 #include <time.h>
+#include <stdbool.h>
+#include <stdlib.h> // size_t
 
+bool ctime_interval_r(struct tm* interval, char* dest, size_t limit);
 const char* ctime_interval(struct tm* interval);
 
 void advance_interval(struct tm* dest, struct tm* interval);
