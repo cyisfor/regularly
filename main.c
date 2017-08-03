2e3ed5ea9c254b5f5ae2c3c5971e87f69f52e9a0
cyisfor <autocommit>
2017-08-03 01:23:37 +0000

    auto 0 18 120

--- main.c
+++ main.c
@@ -88,6 +88,7 @@ static size_t find_point(struct rule* r, size_t num, struct timespec due) {
 			lo = i;
 			if(lo + 1 == hi) break;
 		} else if(timespecequal(&r[i].due,&due)) {
+			lo = i;
 			break;
 		} else {
 			info("point ↑ %d→%d",hi,i);
