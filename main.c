78ec7f9aa3f54088557ec90031f420fea9ff7632
cyisfor <autocommit>
2017-08-03 01:21:46 +0000

    auto 0 34 262

--- main.c
+++ main.c
@@ -97,18 +97,6 @@ static size_t find_point(struct rule* r, size_t num, struct timespec due) {
 	}
 	return lo;
 
-
-		i += step;
-		step = step >> 1;
-	}
-	while(step > 0 && (!timespecbefore(&r[i].due,&due))) {
-		// should be lower
-		i -= step;
-		step = step >> 1;
-	}
-	return i;
-}
-
 static size_t sort_insert(struct rule* r, size_t num, struct timespec due) {
 	size_t i = find_point(r,num,due);
 	// found insertion point. now shift old ones up
