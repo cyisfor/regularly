18f025c40d4a5ca4bfa39ca56cd88be2dc2d34ff
cyisfor <autocommit>
2017-08-03 06:41:41 +0000

    auto 0 25 256

--- main.c
+++ main.c
@@ -518,7 +518,7 @@ int main(int argc, char *argv[])
 		assert_zero(chdir("regularly"));
 		mkdir("logs",0755);
 		// to avoid springing inotify every time a child PID closes its logfd
-		logfd = open("logs/log",O_APPEND|O_WRONLY|O_CREAT,0644);
+		logfd = open("logs/current",O_APPEND|O_WRONLY|O_CREAT,0644);
 		inotify_add_watch(ino,".",IN_MOVED_TO|IN_CLOSE_WRITE);
 	} else {
 		logfd = STDERR_FILENO;
